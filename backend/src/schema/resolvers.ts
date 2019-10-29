cd1780612e4d61930a47b557ddb269a7117aa47e
Toni Pakarinen <toni.pakarinen@eng.tamk.fi>
2019-10-29 18:29:24 +0200

    admin user pw reset done

--- backend/src/schema/resolvers.ts
+++ backend/src/schema/resolvers.ts
@@ -357,6 +357,60 @@ export default {
       const user = await prisma.deleteUser({ id: id });
 
       return user;
+    },
+    // USER PASSWORD RESET
+    userPWReset: async (
+      obj,
+      { input: { id, password, passwordAgain } },
+      { currentUser }
+    ) => {
+      if (!DEBUG) {
+        mustBeLoggedIn(currentUser);
+        mustBeAtleastLevel(currentUser, UserLevels.ADMIN);
+
+        logger.log(
+          "info",
+          "[M USERUPDATEPW] User %s password have been updated by %s",
+          id,
+          currentUser.id
+        );
+      }
+
+      // password legality checks
+      if (
+        !password.replace(/\s/g, "").length ||
+        password === null ||
+        !passwordAgain.replace(/\s/g, "").length ||
+        passwordAgain === null
+      ) {
+        logger.log("warn", "[M USERUPWRESET] Password is null");
+        throw new Error("Password can not be null or empty!");
+      }
+      // bcryptjs max input lenght is 18
+      if (password.length > MAX_PW) {
+        logger.log("warn", "[M USERUPWRESET] Password is too long");
+        throw new Error("Password too long!");
+      }
+      // password min lenght
+      if (password.length < MIN_PW) {
+        logger.log("warn", "[M USERUPWRESET] Password is too short");
+        throw new Error("Password too short!");
+      }
+      if (password !== passwordAgain) {
+        logger.log("warn", "[M USERUPWRESET] Password don't match");
+        throw new Error("Password don't match!");
+      }
+
+      const user = await prisma.updateUser({
+        data: {
+          password: await bcrypt.hash(password, SALT_ROUNDS)
+        },
+        where: {
+          id: id
+        }
+      });
+
+      return { user };
     }
   }
 };
